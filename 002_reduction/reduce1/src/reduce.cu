065a5ac8c3ad9859fb4862f80be492ef5e2098e2
Hang Yang <hyang@birentech.com>
2023-11-30 20:43:44 +0800

    add v1 reduction

---
+++ 002_reduction/reduce1/src/reduce.cu
@@ -0,0 +1,212 @@
+/* Comp 4510 - CUDA Sum Reduction Example
+ *
+ * This code performs a sum reduction on an array of size n = 2^i, where i is
+ * passed in as a command line arg.
+ * It outputs the resulting sum along with some performance statistics.
+ *
+ * This is the initial (naive) implementation. global memory coalescing
+ */
+
+#include "kernels.h"
+#include "perf.h"
+#include "reduce.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// assume dev_input, dev_output are allocated
+void reduce_yh_host(float *dev_input, float *dev_output, int n) {
+  unsigned int remaining = n;
+  unsigned int threads_needed = n / 2;
+  unsigned int blocks = threads_needed / 1024 + (threads_needed % 1024 ? 1 : 0);
+  unsigned int block_threads;
+
+  if (blocks > 0) {
+    block_threads = 1024;
+  } else {
+    block_threads = threads_needed;
+    blocks = 1;
+  }
+
+  while (remaining > 1) {
+#if DEBUG_INFO
+    printf("Launching kernels:\n");
+    printf("remaining: %u\n", remaining);
+    printf("blocks: %u\n", blocks);
+    printf("threads_needed: %u\n", threads_needed);
+    printf("\n");
+#endif
+    reduce_yh<<<blocks, block_threads>>>(dev_input, dev_output, remaining);
+    remaining = blocks;
+    threads_needed = remaining / 2;
+    blocks = threads_needed / 1024;
+    if (blocks > 0) {
+      block_threads = 1024;
+    } else {
+      block_threads = threads_needed;
+      blocks = 1;
+    }
+    if (remaining > 1) {
+      float *dev_tmp = dev_input;
+      dev_input = dev_output;
+      dev_output = dev_tmp;
+    }
+    // cudaDeviceSynchronize();
+  }
+}
+
+void reduce_host(float *dev_input, float *dev_output, int n) {
+  unsigned int remaining = n; // tracks number of elements left to add
+  // Size info for kernel launches
+  unsigned int threads_needed;
+  unsigned int blocks;
+  const int block_threads = get_max_block_threads();
+
+  float *dev_temp;        // temp pointer used to flip buffers between kernel
+                          // launches (see loop below)
+  threads_needed = n / 2; // we'll need one thread to add every 2 elements
+  blocks = threads_needed / block_threads + \ // we'll need this many blocks
+           (threads_needed % block_threads > 0
+                ? 1
+                : 0); // plus one extra if threads_needed
+                      // does not evenly divide block_threads
+  while (remaining >
+         1) // continue until we have a single value left (the final sum)
+  {
+#if DEBUG_INFO
+    printf("Launching kernels:\n");
+    printf("remaining: %u\n", remaining);
+    printf("blocks: %u\n", blocks);
+    printf("threads_needed: %u\n", threads_needed);
+    printf("\n");
+#endif
+
+    // call the kernel
+    // reduce<<<blocks, block_threads>>>(dev_input, dev_output, remaining);
+    reduce<<<blocks, block_threads>>>(dev_input, dev_output, remaining);
+
+    // re-compute our size information for the next iteration
+    remaining = blocks; // After the previous kernel call, each block has
+                        // reduced its chunk down to a single partial sum
+    threads_needed = remaining / 2; // each thread added 2 elements
+    blocks = threads_needed / block_threads +
+             (threads_needed % block_threads
+                  ? 1
+                  : 0); // again, might need one extra block if threads_needed
+                        // is not evenly divisible by block_threads
+
+    // if we will need to do another iteration, flip (swap) the device input and
+    // output buffers; i.e. the output buffer from the last call becomes input
+    // buffer for the next call, and the input buffer from last call is re-used
+    // to store output for the next call. Note: no data is transferred back to
+    // the host here, this is just a pointer operation
+    if (remaining > 1) {
+      dev_temp = dev_input;
+      dev_input = dev_output;
+      dev_output = dev_temp;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  // Grab n from the command line args
+  const unsigned int n = parse_args(argc, argv);
+
+  // Query device to find max num threads per block
+  const int block_threads = get_max_block_threads();
+  printf("max threads per block: %d\n", block_threads);
+
+  // Prepare timer objects to track performance
+  Perf perf = create_perf();
+
+  // All CUDA API calls return a status, which we must check
+  cudaError_t status;
+
+  // Host buffers
+  float *host_array;
+  float final_sum = 0.001;
+
+  // Device buffers
+  float *dev_input;
+  float *dev_output;
+
+  // Size info for kernel launches
+  unsigned int threads_needed;
+  unsigned int blocks;
+  unsigned int remaining; // number of elements left to add
+
+  // Allocate host buffer and fill it
+  host_array = (float *)malloc(n * sizeof(float));
+  init_array(host_array, n);
+
+  // Allocate device buffers
+  // Note: Input buffer needs to be of size n.
+  // Output buffer is used to store partial results after each kernel launch. On
+  // first launch, each block will reduce block_size * 2 values down to 1 value
+  // (except last kernel, which may reduce less than block_size * 2 values down
+  // to 1 value if  n is not a multiple of block_size * 2). On subsequent
+  // launches, we need even less output buffer space. Therefore output buffer
+  // size needs to be equal to the number of blocks required for first launch.
+
+  status = cudaMalloc(&dev_input, n * sizeof(float));
+  check_error(status, "Error allocating device buffer.");
+  status = cudaMalloc(&dev_output, blocks * sizeof(float));
+  check_error(status, "Error allocating device buffer.");
+
+  // Start the program timer
+  start_timer(&(perf.total_timer));
+
+  // Transfer the input array from host to device
+  start_timer(&(perf.h2d_timer));
+  status = cudaMemcpy(dev_input, host_array, n * sizeof(float),
+                      cudaMemcpyHostToDevice);
+  stop_timer(&(perf.h2d_timer));
+  check_error(status, "Error on CPU->GPU cudaMemcpy for host_array.");
+
+  // Launch kernel
+  // Note: We call the kernel multiple times - each call reduces the size of the
+  // array by 2.
+  start_timer(&(perf.kernel_timer));
+#if USE_YH_IMPL
+  reduce_yh_host(dev_input, dev_output, n);
+#else
+  reduce_host(dev_input, dev_output, n);
+#endif
+  stop_timer(&(perf.kernel_timer));
+  // Note: the kernel launches in the loop above are asychronous, so this may
+  // not necessarily catch kernel errors... If they're not caught here, they'll
+  // be caught in the check_error() call after the next blocking operation (the
+  // GPU -> CPU data transfer below).
+  check_error(cudaGetLastError(), "Error launching kernel.");
+
+  // Transfer the element in position 0 of the dev_output buffer back to the
+  // host. This is the final sum.
+  start_timer(&(perf.d2h_timer));
+  status =
+      cudaMemcpy(&final_sum, dev_output, sizeof(float), cudaMemcpyDeviceToHost);
+  stop_timer(&(perf.d2h_timer));
+  check_error(status, "Error on GPU->CPU cudaMemcpy for final_sum.");
+
+  // Record the final clock time
+  stop_timer(&(perf.total_timer));
+  // Since the GPU operates asynchronously, wait until the final time has been
+  // recorded before continuing on to print the results below (this synchronizes
+  // the device and the host).
+  cudaEventSynchronize(perf.total_timer.stop);
+
+  // Display the results & performance statistics
+  print_results(n, final_sum, &perf);
+  float golden_sum = 0.f;
+  cpu_reduce(host_array, n, &golden_sum);
+  check_result(golden_sum, final_sum);
+
+  // Clean up memory (both on host *AND device*!)
+  free(host_array);
+  destroy_perf(&perf);
+  status = cudaFree(dev_input);
+  check_error(status, "Error calling cudaFree on device buffer.");
+  status = cudaFree(dev_output);
+  check_error(status, "Error calling cudaFree on device buffer.");
+
+  return EXIT_SUCCESS;
+}
--- 002_reduction/reduction_readme.md
+++ 002_reduction/reduction_readme.md
@@ -1,7 +1,29 @@
+- [1. Tips](#1-tips)
+- [2. reduce v0](#2-reduce-v0)
+  - [2.1. host 侧 kernel launch 调用](#21-host-侧-kernel-launch-调用)
+  - [2.2. reduce kernel](#22-reduce-kernel)
+  - [2.3. Summary](#23-summary)
+- [3. reduce v1](#3-reduce-v1)
+  - [3.1. kernel](#31-kernel)
+
+
+
 [medium, Parallel Reduction with CUDA](https://shreeraman-ak.medium.com/parallel-reduction-with-cuda-d0ae10c1ae2c)
 [github, cuda-reduction-example](https://github.com/umfranzw/cuda-reduction-example), has slides inside
 
-## reduce v0
+
+## 1. Tips
+
+问题
+- 实现层面，需要在该 sync 的地方记得 sync
+- host 端每次调用要对调 input, output ? 是的，因为 kernel 假定 input 是连续的，如果不交换的话，input 里的部分和是不连续的。
+- 交换的话，最后拿到的结果不是 dev_output 怎么办？没事，因为kernel 里面已经将结果都写入 input，output buffer，两份中都有 final sum。
+- V0 实现会导致 input 被破坏，不好，最好是不用 inplace 做法。
+- 算法层面犯了一个大错，即一 loop 中，不同 thread 读写到相同位置了。至于为什么第二次 launch 出问题的根本原因还没搞清楚。毕竟也多加了同步了。
+
+
+
+## 2. reduce v0
 
 基本算法结构分为两层，一层在 host 侧，一层在 device 侧，device 侧只做 block 内 reduce，host 侧循环调用 device reduce，直到 reduce 到一个值。
 
@@ -16,7 +38,7 @@
 > 注意接口只支持 int32_t ，所以输入参数超过 31 将溢出。
 
 
-### host 侧 kernel launch 调用
+### 2.1. host 侧 kernel launch 调用
 
 kernel launch 的次数 $log_{2 * max_block_threads}(n)$，即 2^11, 2^22, 2^33 分别只需要 launch 1，2，3 次 kernel。
 
@@ -33,13 +55,13 @@ block_threads 可以固定为 1024，即 cuda 的最多线程数量。在 kernel
 cudaDeviceGetAttribute(&max_threads, cudaDevAttrMaxThreadsPerBlock, dev_num);
 ```
 
-### reduce kernel
+### 2.2. reduce kernel
 
 输入一个结果，输出到紧排列的 output buffer，input 也被改了。一个 thread block 作用域 block size * 2 个数，即 chunk size = block size * 2。
 
 ```cpp
 // 每个 block 会作用到 2 * block_size 的数据
-for (stride = 1; stride < chunk_size; stride *= 2, threads /= 2) {
+for (stride = 1; thread > 0; stride *= 2, threads /= 2) {
     left = block_start + id * (stride * 2)
     right = left + stride
 
@@ -51,7 +73,86 @@ for (stride = 1; stride < chunk_size; stride *= 2, threads /= 2) {
 }
 ```
 
-### Summary
+> 注意代码实现中要同步
+
+![naive impl](picture/v0_algorithm.png)
+
+### 2.3. Summary
 
 - 为何要多次 launch kernel，不直接在 kernel 中完成 reduce 工作？
 kernel 内部不能在 block 之间做 reduce，所以需要返回 host 侧，从而实现 block 之间同步。
+
+缺点
+- input 数据在 global memory，被多次读取，成本较高
+
+
+## 3. reduce v1
+
+做 global memory coalescing
+
+上一版 block 中后面几个 loop 取点的成本较高，可以将他们紧排列，优化数据读取。
+
+
+![global memory coalescing](picture/v1_algorithm.png)
+
+### 3.1. kernel
+
+下面的 kernel 在多次 launch 时会出现 mismatch，原因不明。可能是不同 thread 对同一个地址进行了读写。
+
+```cpp
+  unsigned int block_size = blockDim.x; // 1024 or n when n < 1024
+  unsigned int thread_id = threadIdx.x;
+  unsigned int block_id = blockIdx.x;
+  unsigned int threads = block_size;
+
+  // The size of the chunk of data this thread's block is working on.
+  unsigned int chunk_size = block_size * 2;
+  unsigned int block_start = block_id * chunk_size;
+
+  unsigned int left, right, loop_out;
+
+  for (; threads > 0; threads /= 2) {
+    left = block_start + thread_id * 2;
+    right = left + 1;
+    loop_out = block_start + thread_id;
+    __syncthreads();
+
+    if (thread_id < threads && right < n) {
+      input[loop_out] = input[left] + input[right];
+    }
+    __syncthreads();
+  }
+```
+
+改正后按照上图中的算法如下：
+
+```cpp
+__global__ void reduce_yh(float *input, float *output, unsigned int n) {
+  unsigned int block_size = blockDim.x; // 1024 or n when n < 1024
+  unsigned int thread_id = threadIdx.x;
+  unsigned int block_id = blockIdx.x;
+  unsigned int threads = block_size;
+
+  // The size of the chunk of data this thread's block is working on.
+  unsigned int chunk_size = block_size * 2;
+  unsigned int block_start = block_id * chunk_size;
+
+  unsigned int left, right, stride = block_size;
+
+  for (; threads > 0; threads /= 2, stride /= 2) {
+    left = block_start + thread_id;
+    right = left + stride;
+
+    if (thread_id < threads && right < n) {
+      input[left] += input[right];
+    }
+    __syncthreads();
+  }
+
+  if (thread_id == 0) {
+    // printf("block_size: %d, block_id: %d, n: %d\n", block_size, block_id,
+    // n);
+    output[block_id] = input[block_start];
+  }
+}
+```
